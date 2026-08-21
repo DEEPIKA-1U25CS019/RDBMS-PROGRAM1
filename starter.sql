8da8450e076a0992378fb39066fb790eb96556a2
DEEPIKA-1U25CS019 <deepikaspdeepikasp163@gmail.com>
2026-08-21 11:24:52 +0530

    Rename database and modify department table creation

--- starter.sql
+++ starter.sql
@@ -1,5 +1,5 @@
-create database collegedb;
+create database colldb;
 use db;
-create table department(deptID integer(10) primary key,depName varchar(28),HOD varchar(25) not null);
+create table department(deptID integer(10) primary key,depName varchar(28),HOD varchar(25));
 desc department;
 
